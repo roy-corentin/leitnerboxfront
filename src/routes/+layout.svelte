f2a9f4da67e6c834442259513f045a005c573bcd
roy-corentin <jilkonx@laposte.net>
2023-10-01 14:01:53 +0200

    [UP] better navbar with daisyUI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,11 @@
   import "../app.css";
 </script>
 
-<nav>
-  <a href="/">Home</a>
+<nav class="navbar bg-base-100 border-b-2 border-primary-focus mb-4">
+  <a class="navbar-start link-primary text-lg" href="/">LeitnerBox</a>
+  <div class="navbar-end gap-3">
+    <a class="text-accent" href="/login">Login</a>
+    <a class="text-info" href="/sign-up">Sign-up</a>
+  </div>
 </nav>
 <slot />
