b4cf767b6151191464ffafd8cb3e2f7860885be3
roy-corentin <jilkonx@laposte.net>
2023-10-01 15:36:34 +0200

    [ADD] component Navbar

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,15 @@
+<script>
+  import "../app.css";
+</script>
+
+<nav class="navbar bg-base-100 border-b-2 border-primary-focus mb-4">
+  <a class="navbar-start link-primary text-lg" href="/">LeitnerBox</a>
+  <div class="navbar-end gap-3">
+    <div class="border p-2 border-accent rounded-md">
+      <a class="text-accent" href="/login">Login</a>
+    </div>
+    <div class="border p-2 border-secondary rounded-md">
+      <a class="text-secondary" href="/sign-up">Sign-up</a>
+    </div>
+  </div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,6 @@
 <script>
-  import "../app.css";
+  import Navbar from "$lib/Navbar.svelte";
 </script>
 
-<nav class="navbar bg-base-100 border-b-2 border-primary-focus mb-4">
-  <a class="navbar-start link-primary text-lg" href="/">LeitnerBox</a>
-  <div class="navbar-end gap-3">
-    <a class="text-accent" href="/login">Login</a>
-    <a class="text-info" href="/sign-up">Sign-up</a>
-  </div>
-</nav>
+<Navbar />
 <slot />
