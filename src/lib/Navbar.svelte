4716271238c9feb51dcf423e18251dcecef9fa7e
roy-corentin <jilkonx@laposte.net>
2023-10-01 22:19:41 +0200

    [FIX] home button to large

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <nav class="navbar bg-base-100 border-b-2 border-primary-focus mb-4">
-  <a class="navbar-start link-primary text-lg" href="/">LeitnerBox</a>
+  <div class="navbar-start">
+    <a class="link-primary text-lg" href="/">LeitnerBox</a>
+  </div>
   <div class="navbar-end gap-3">
     <a class="border p-2 border-accent rounded-md hover:bg-accent text-accent hover:text-accent-content" href="/login"
       >Login</a
