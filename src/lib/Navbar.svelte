98c5bdb8aab01af14c07a5cf1973730afb84e1b2
roy-corentin <jilkonx@laposte.net>
2023-10-01 22:13:41 +0200

    [ADD] hover style for navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -5,11 +5,12 @@
 <nav class="navbar bg-base-100 border-b-2 border-primary-focus mb-4">
   <a class="navbar-start link-primary text-lg" href="/">LeitnerBox</a>
   <div class="navbar-end gap-3">
-    <div class="border p-2 border-accent rounded-md">
-      <a class="text-accent" href="/login">Login</a>
-    </div>
-    <div class="border p-2 border-secondary rounded-md">
-      <a class="text-secondary" href="/sign-up">Sign-up</a>
-    </div>
+    <a class="border p-2 border-accent rounded-md hover:bg-accent text-accent hover:text-accent-content" href="/login"
+      >Login</a
+    >
+    <a
+      class="border p-2 border-secondary rounded-md hover:bg-secondary text-secondary hover:text-secondary-content"
+      href="/sign-up">Sign-up</a
+    >
   </div>
 </nav>
