f3cf4c61e52360ac59c09e0cbaad010dff41ea4a
roy-corentin <jilkonx@laposte.net>
2023-10-01 22:14:38 +0200

    [ADD] test call api to get user info to show them on index

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,17 @@
+import { fail } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ parent }) => {
+type MeJsonResponse = { email: string } | string;
+
+export const load: PageServerLoad = async ({ parent, cookies }) => {
   await parent();
+  const sessionId = cookies.get("sessionId");
+  const response = await fetch("http://localhost:3000/me", {
+    method: "GET",
+    headers: { Authorization: `Bearer ${sessionId}` },
+  });
+  const json: MeJsonResponse = await response.json();
+  console.log(json);
+  if (response.ok) return { user: json };
+  else return fail(400, { error: true, message: json });
 };
