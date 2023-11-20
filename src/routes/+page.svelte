230c4b06d374a9c2d4068d583ec80b9ab583046a
roy-corentin <jilkonx@laposte.net>
2023-11-20 22:36:43 +0100

    [ADD] grid to display futur box + button to add box

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   export let data;
+  import NewBoxButton from "$lib/box/ButtonNew.svelte";
   console.log(data);
 </script>
 
@@ -8,4 +9,7 @@
   {#if data?.user}
     <p>{data?.user?.email}</p>
   {/if}
+  <div class="mt-20 grid md:grid-cols-4 place-items-center md:place-items-strecth gap-8">
+    <NewBoxButton />
+  </div>
 </body>
