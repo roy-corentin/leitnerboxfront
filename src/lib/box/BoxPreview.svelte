a75d6d5aeb943a2c848c38c9e24865801c59b030
roy-corentin <corentin.roy02@laposte.net>
2024-02-15 22:22:07 +0100

    [ADD] link to box on boxPreview

--- src/lib/box/BoxPreview.svelte
+++ src/lib/box/BoxPreview.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
-  export let box: { name: string };
+  export let box: LeitnerBox;
   export let index: number;
 </script>
 
 <body>
-  <div
-    class="group flex flex-col border-2 border-primary hover:border-secondary h-80 w-64 items-center justify-center rounded-xl"
-  >
-    <p class="text-xl font-bold font-mono text-primary group-hover:text-secondary">
-      {`${box.name} ${index + 1}`}
-    </p>
-  </div>
+  <a href={`/box/${box.id}`}>
+    <div
+      class="group flex flex-col border-2 border-primary hover:border-secondary h-80 w-64 items-center justify-center rounded-xl"
+    >
+      <p class="text-xl font-bold font-mono text-primary group-hover:text-secondary">
+        {`${box.name} ${index + 1}`}
+      </p>
+    </div>
+  </a>
 </body>
