6ac5797769a2563ca640bc32cc870868af28d388
roy-corentin <corentin.roy02@laposte.net>
2024-02-24 15:51:53 +0100

    [UP] use grid instead of flex

--- src/routes/box/[id]/+page.svelte
+++ src/routes/box/[id]/+page.svelte
@@ -5,7 +5,6 @@
   export let data: PageData;
   let { box } = data;
   console.log(box);
-  if (box && box.decks && box.decks[0]) box.decks[0].cardIds = ["1", "2", "3", "4"];
 </script>
 
 <div class="flex flex-col items-center justify-center mt-20">
@@ -15,7 +14,7 @@
     <h1 class="text-4xl">Box not found</h1>
   {:else}
     <h1 class="text-4xl mb-10">{box.name}</h1>
-    <div class="flex gap-2">
+    <div class="grid grid-cols-3 md:grid-cols-5 gap-3">
       {#each box.decks as deck}
         <DeckPreview {deck} />
       {/each}
