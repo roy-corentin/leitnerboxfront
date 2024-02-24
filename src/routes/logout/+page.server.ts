642559077147298cf5e2a5644f4e785d904ccb65
roy-corentin <corentin.roy02@laposte.net>
2024-02-24 21:06:18 +0100

    [UP] logout set user and sessionId to null

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -1,7 +1,9 @@
 import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ cookies }) => {
+export const load: PageServerLoad = async ({ cookies, locals }) => {
+  locals.user = null;
+  locals.sessionId = null;
   cookies.delete("sessionId", { path: "/" });
   throw redirect(302, "/login");
 };
