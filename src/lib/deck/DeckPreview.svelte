8433fc185ea7b99a618430cf56f7acf9f32c1b55
roy-corentin <corentin.roy02@laposte.net>
2024-02-24 15:24:19 +0100

    [ADD] Deck Card Preview

---
+++ src/lib/deck/DeckPreview.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let deck: Deck;
+  const colors = ["secondary", "accent", "blue-400", "red-400", "green-400"];
+</script>
+
+<div class="w-36 card border flex items-center">
+  <h3 class="card-title">{deck.level}</h3>
+  <div class="card-body">
+    <div class="stack">
+      <div class="grid w-32 h-20 rounded bg-primary text-primary-content place-content-center">
+        {deck.cardIds.length}
+      </div>
+      {#each deck.cardIds as _cardId, index}
+        <div class="grid w-32 h-20 rounded bg-{colors[index % colors.length]}" />
+      {/each}
+    </div>
+  </div>
+  <div class="card-actions form-control my-2">
+    <input type="text" class="select select-xs w-full" value={deck.periodType} placeholder="period_type" />
+    <input type="text" class="input input-xs w-full" value={deck.periodUnit} placeholder="period_unit" />
+  </div>
+</div>
--- src/routes/box/[id]/+page.svelte
+++ src/routes/box/[id]/+page.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
+  import DeckPreview from "$lib/deck/DeckPreview.svelte";
   import type { PageData } from "./$types";
 
   export let data: PageData;
   let { box } = data;
   console.log(box);
+  if (box && box.decks && box.decks[0]) box.decks[0].cardIds = ["1", "2", "3", "4"];
 </script>
 
-<div class="flex items-center justify-center mt-20">
+<div class="flex flex-col items-center justify-center mt-20 border">
   {#if box === undefined}
     <div class="loading loading-dots loading-lg" />
   {:else}
-    <h2>{box.name}</h2>
+    <h1 class="text-4xl mb-10">{box.name}</h1>
+    <div class="flex gap-2">
+      {#each box.decks as deck}
+        <DeckPreview {deck} />
+      {/each}
+    </div>
   {/if}
 </div>
