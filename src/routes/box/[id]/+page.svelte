6c315035a2bfda37f87af6209d2a220c626b6b09
roy-corentin <corentin.roy02@laposte.net>
2024-02-24 21:07:49 +0100

    [ADD] link to create new card

--- src/routes/box/[id]/+page.svelte
+++ src/routes/box/[id]/+page.svelte
@@ -7,17 +7,18 @@
   console.log(box);
 </script>
 
-<div class="flex flex-col items-center justify-center mt-20">
+<div class="flex flex-col items-center justify-center px-20 mt-20">
   {#if box === undefined}
     <div class="loading loading-dots loading-lg" />
   {:else if box === null}
     <h1 class="text-4xl">Box not found</h1>
   {:else}
     <h1 class="text-4xl mb-10">{box.name}</h1>
-    <div class="grid grid-cols-3 md:grid-cols-5 gap-3">
+    <div class="grid grid-cols-3 md:grid-cols-5 gap-4">
       {#each box.decks as deck}
         <DeckPreview {deck} />
       {/each}
     </div>
+    <a href={`/box/{box.id}/create-card`} class="btn mt-10 w-full bg-accent text-accent-content">New Card</a>
   {/if}
 </div>
