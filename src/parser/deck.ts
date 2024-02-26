92efaf46575532e8117f5a67b9fe8a9ba32495c9
roy-corentin <corentin.roy@epitech.eu>
2024-02-26 11:51:45 +0100

    [UP] parse deck last_review date

--- src/parser/deck.ts
+++ src/parser/deck.ts
@@ -6,18 +6,20 @@ export interface DeckApi {
     period_unit: number;
     period_type: PeriodType;
     level: number;
-    last_review: Date | null;
+    last_review: string | null;
     card_ids: string[];
 }
 
 export function parseDeck(deckApi: DeckApi): Deck {
+    const lastReview = deckApi.last_review ? new Date(deckApi.last_review) : null;
+
     return {
         id: deckApi.id,
         leitnerBoxId: deckApi.leitner_box_id,
         periodUnit: deckApi.period_unit,
         periodType: deckApi.period_type,
         level: deckApi.level,
-        lastReview: deckApi.last_review,
+        lastReview,
         cardIds: deckApi.card_ids,
     };
 }
